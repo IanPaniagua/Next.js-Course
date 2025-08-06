b330978eb2383faa8a24a2e646e4c5e2fcaaf07c
IanPaniagua <paniagua.ian.de@gmail.com>
2025-08-06 13:45:20 +0200

    Component counter shopping cart + metadata

--- 02-manolo-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-manolo-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,8 +1,20 @@
-export default function NamePage() {
+import { CartCounter } from "@/app/components/shopping-cart/components/CartCounter";
+
+export const metadata = {
+  title: 'Shopping Cart Counter',
+  description: 'Local state management example using React hooks'
+};
+
+export default function CounterPage() {
+
+  //Aqui podriamos tener una conexion a base de datos.
+  // y el valor podria ser generado desde la BS
+ 
 
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full ">
+      <span>Productos en el carrito</span>
+      <CartCounter value={20}/>
     </div>
   );
 }
